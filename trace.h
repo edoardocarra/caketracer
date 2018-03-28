4984d53776f698471b3422157f2ebae611311eef
Edoardo Carra <carra@di.uniroma1.it>
2018-03-28 16:08:42 +0200

    .

--- trace.h
+++ trace.h
@@ -1,9 +1,24 @@
 #include <iostream>
+#include "yocto/yocto_gl.h"
 
 #ifndef _TRACE_H_
 #define _TRACE_H_
 
 namespace r_tracer {
+
+    struct ray{
+
+
+        ygl::vec3f origin() {return A};
+        ygl::vec3f direction() {return A};
+        ygl::vec3f ray_point(float t) {return A + t*B};
+
+        ygl::vec3f A;
+        ygl::vec3f B;
+
+
+    };
+
 
     void raytrace();
 
